55aa03f8fc9c0ee02a4ea6795068d6d3a8064f27
Woofer <61364477+Woofer21@users.noreply.github.com>
2026-02-25 16:38:19 -0500

    feat: init component 'lib'

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Page() {
 	return (
-		<div className="flex min-h-screen items-center justify-center">
-			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+		<div>
+			<main>
 				<h1>SemesterSync</h1>
 			</main>
 		</div>
