f208d3c08d68c6244e60e5a3e1e6cb648216c794
Woofer <61364477+Woofer21@users.noreply.github.com>
2026-03-05 12:16:34 -0500

    chore: format & lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import AppHeader from "@/components/app-header";
 import { Button } from "@/components/ui/button";
@@ -7,7 +7,7 @@ import useUserStore from "@/stores/user-store";
 export default function Page() {
 	const tabs = useUserStore((state) => state.tabs);
 	const addTab = useUserStore((state) => state.addTab);
-	
+
 	return (
 		<div className="w-full">
 			<AppHeader />
