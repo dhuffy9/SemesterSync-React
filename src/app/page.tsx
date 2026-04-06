d1dbbc5dc90fe9b64e78885e965c07c0f3245129
= <djh61@pct.edu>
2026-04-05 21:48:33 -0400

    New branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import AppHeader from "@/components/header/app-header";
 import TabList from "@/components/tab-list";
+import CalendarContainer from "@/components/calendar-container";
 
 export default async function Page() {
 	return (
-		<div className="w-full">
+		<div>
 			<AppHeader />
 
 			<main>
 				<TabList />
+			    <CalendarContainer/>
 			</main>
 		</div>
 	);
---
+++ src/components/calendar-container.tsx
@@ -0,0 +1,33 @@
+export default function ClassList() {
+	const cols = 7;
+	const rows = 85;
+
+	return (
+            <div className="flex flex-row w-full overflow-hidden">
+                <div className="w-[80px]">
+
+                </div>
+                <div className="flex flex-col flex-1 min-w-0">
+                    <div className="w-full h-[100px]">
+
+                    </div>
+                    <div className="grid grid-cols-7 w-full  min-w-0">
+                        {Array.from({ length: rows * cols }).map((_, i) => {
+                            const col = i % cols;
+                            const row = Math.floor(i / cols);
+                            
+                            return (
+                                <div
+                                    key={`${col}-${row}`}
+                                    className="border h-5 text-xs flex items-center justify-center"
+                                >
+                                            {col},{row}
+            
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+	);
+}
