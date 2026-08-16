dda86a6b516b94d917dbe92b211fab8c7ef5007c
Tobias <61364477+tobias-hay@users.noreply.github.com>
2026-08-16 19:05:51 -0400

    chore(mini-cal): improve highlighting of what full cal shows

--- src/components/sidebar/miniCalendar/mini-calendar.tsx
+++ src/components/sidebar/miniCalendar/mini-calendar.tsx
@@ -1,79 +1,84 @@
 "use client";
 
+import clsx from "clsx";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import useUserStore from "@/stores/user-store";
 
+const monthNames = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
+
 export default function MiniCalendar() {
 	const activeTab = useUserStore((state) => state.getActiveTab());
-	const updataTabData = useUserStore((state) => state.updateTabDate);
+	const updataTabDate = useUserStore((state) => state.updateTabDate);
 
-	const [selectedDay, setSelectedDay] = useState<number | null>(null);
-
-	const monthNames = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	];
-
-	const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
-
+	const [selectedDate, setSelectedDate] = useState<Date>(
+		activeTab.selectedDate ? new Date(activeTab.selectedDate) : new Date(),
+	);
 	const today = new Date();
+
+	useEffect(() => {
+		if (activeTab.selectedDate) {
+			setSelectedDate(new Date(activeTab.selectedDate));
+		}
+	}, [activeTab.selectedDate]);
 
 	const changeMonth = (direction: number) => {
 		const nextDate = new Date(selectedDate);
-		nextDate.setMonth(nextDate.getMonth() + direction, selectedDate.getDay());
-		updataTabData(activeTab.id, nextDate);
-	};
 
-	const getDaysInMonth = (year: number, month: number) => {
-		return new Date(year, month + 1, 0).getDate(); // months start at 0
-	};
+		let day = nextDate.getDate();
+		if (day === getDaysInMonth(nextDate.getMonth()))
+			day = getDaysInMonth(nextDate.getMonth() + direction);
 
-	const getFirstDayOfMonth = (year: number, month: number) => {
-		return new Date(year, month, 1).getDay();
+		nextDate.setMonth(nextDate.getMonth() + direction, day);
+
+		setSelectedDate(nextDate);
+		updataTabDate(activeTab.id, nextDate);
 	};
 
 	const goToDay = (day: number) => {
-		setSelectedDay(day);
-		updataTabData(
-			activeTab.id,
-			new Date(selectedDate.getFullYear(), selectedDate.getMonth(), day),
+		const date = new Date(
+			selectedDate.getFullYear(),
+			selectedDate.getMonth(),
+			day,
 		);
+		setSelectedDate(date);
+		updataTabDate(activeTab.id, date);
 	};
-
-	const selectedDate = activeTab.selectedDate
-		? new Date(activeTab.selectedDate)
-		: new Date();
 
 	const year = selectedDate.getFullYear();
 	const month = selectedDate.getMonth();
 
-	const daysInMonth = getDaysInMonth(year, month);
-	const firstDay = getFirstDayOfMonth(year, month);
+	const daysInMonth = getDaysInMonth(month);
 
 	const calendarDays = [];
+	const dateToRow = new Map<number, number>();
 
-	for (let i = 0; i < firstDay; i++) {
-		calendarDays.push(null);
-	}
-
+	let row = 1;
 	for (let i = 1; i <= daysInMonth; i++) {
-		calendarDays.push(i);
+		const date = new Date(year, month, i);
+		dateToRow.set(i, row);
+		calendarDays.push(date);
+		if (date.getDay() === 6) row++;
 	}
 
 	return (
-		<div className="">
+		<div>
 			<div className="flex justify-between pt-2 pb-2">
 				<Button
 					onClick={() => changeMonth(-1)}
@@ -98,30 +103,52 @@ export default function MiniCalendar() {
 					<span key={`${index}_${day}`}>{day}</span>
 				))}
 			</div>
-			<div className="grid grid-cols-7  text-center">
+			<div
+				className="grid grid-cols-7 text-center"
+				style={{
+					gridTemplateRows: `repeat(${dateToRow
+						.entries()
+						.toArray()
+						.pop()}, mix-max(0, 1fr))`,
+				}}
+			>
+				<div
+					className="col-span-full border border-muted-foreground/40 border-dashed rounded-md"
+					style={{
+						gridRowStart: dateToRow.get(selectedDate.getDate()),
+					}}
+				/>
 				{calendarDays.map((day, index) => {
 					const isSelected =
-						day === today.getDate() &&
+						day.getDate() === today.getDate() &&
 						month === today.getMonth() &&
 						year === today.getFullYear();
-
-					if (day == null) {
-						return <span key={`${index}_${day}`}></span>;
-					}
 
 					return (
 						<Button
 							key={`${index}_${day}`}
 							variant={isSelected ? "default" : "ghost"}
 							size={"lg"}
-							className={`${selectedDay === day ? "border-dashed border-primary" : ""}rounded-full font-normal`}
-							onClick={() => goToDay(day)}
+							className={clsx("font-normal rounded-full", {
+								"border-dashed border-primary rounded-md":
+									selectedDate.getDate() === day.getDate(),
+							})}
+							style={{
+								gridColumnStart: day.getDay() + 1,
+								gridRowStart: dateToRow.get(day.getDate()),
+							}}
+							onClick={() => goToDay(day.getDate())}
 						>
-							{day}
+							{day.getDate()}
 						</Button>
 					);
 				})}
 			</div>
 		</div>
 	);
+}
+
+function getDaysInMonth(month: number) {
+	return new Date(2026, month + 1, 0).getDate();
+	// wtf? setting day to 0 bumps you to last day of previous month
 }
